a198bac84d0f285a37f3a119ec4e75c6b76eb152
tdedinsk <thomas.dedinsky@uwaterloo.ca>
2021-01-08 17:02:59 -0500

    Initial Hello World application

---
+++ fydp-web/src/main/java/com/thomasdedinsky/fydp/fydpweb/FydpWebApplication.java
@@ -0,0 +1,13 @@
+package com.thomasdedinsky.fydp.fydpweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FydpWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FydpWebApplication.class, args);
+	}
+
+}
---
+++ fydp-web/src/test/java/com/thomasdedinsky/fydp/fydpweb/FydpWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomasdedinsky.fydp.fydpweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FydpWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
