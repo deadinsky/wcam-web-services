84f0d3df9e2c073a62692f70765bea46b7f2ad8e
tdedinsk <thomas.dedinsky@uwaterloo.ca>
2021-03-07 17:28:14 -0500

    Formatted forms

--- fydp-web/src/main/java/com/thomasdedinsky/fydp/fydpweb/auth/SecurityConfig.java
+++ fydp-web/src/main/java/com/thomasdedinsky/fydp/fydpweb/auth/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests().antMatchers("/", "/index",
-                    "/webjars/**",
+                    "/style.css", "/webjars/**",
                     "/alerts/refresh/*", "/signup", "/confirm").permitAll()
                 .anyRequest().authenticated()
                 .and()
